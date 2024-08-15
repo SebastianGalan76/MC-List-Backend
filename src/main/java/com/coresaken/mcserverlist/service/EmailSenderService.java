247f57fa44fd378588a8c1da9c06f5d74dc283fe
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-08-15 18:55:25 +0200

    Changed emails

--- src/main/java/com/coresaken/mcserverlist/service/EmailSenderService.java
+++ src/main/java/com/coresaken/mcserverlist/service/EmailSenderService.java
@@ -24,10 +24,10 @@ public class EmailSenderService {
                 + "    <div id='header' style='background-color: #1f1f7c; width: 100%; height: 250px;'></div>"
                 + "    <div id='panel' style='background-color: white; width: 700px; margin-left: auto; margin-right: auto; margin-top: -100px; border-radius: 5px;'>"
                 + "        <div id='logo' style='width: 250px; padding: 25px; margin-left: auto; margin-right: auto;'>"
-                + "            <img src='https://zapodaj.net/images/fb31beff19dd9.png' style='width: 100%; height: auto;'>"
+                + "            <img src='https://zapodaj.net/images/f86bc94eb0cbe.png' style='width: 100%; height: auto;'>"
                 + "        </div>"
                 + "        <div id='container' style='padding: 25px'>"
-                + "            Dziękujemy za zarejestrowanie się na <span style='font-weight: 800;'>JoinGameDev.pl!</span><br><br><br>"
+                + "            Dziękujemy za zarejestrowanie się na <span style='font-weight: 800;'>MC-List.pl!</span><br><br><br>"
                 + "            Aby zakończyć proces rejestracji i aktywować swoje konto, prosimy o kliknięcie poniższy przycisk:"
                 + "            <a href='"+activeAccountLink+"' style='text-decoration: none;'>"
                 + "                <div id='button' style='background-color: #ffbe4d; padding: 10px 50px; width: fit-content; border-radius: 5px; color: white; font-weight: 800; margin-top: 20px; margin-bottom: 30px; margin-left: auto; margin-right: auto;'>"
@@ -41,7 +41,7 @@ public class EmailSenderService {
                 + "    </div>"
                 + "    <div id='panel-down' style='background-color: #d1d1d1; padding: 25px 50px; width: 600px; margin-left: auto; margin-right: auto; margin-top: 50px; margin-bottom: 150px; text-align: center;'>"
                 + "        Jeśli potrzebujesz pomocy, prosimy o kontakt <br>"
-                + "        <span style='font-weight: 800;'>support@joingamedev.pl</span>"
+                + "        <span style='font-weight: 800;'>support@coresaken.com</span>"
                 + "    </div>"
                 + "</body>"
                 + "</html>";
@@ -51,8 +51,8 @@ public class EmailSenderService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         helper.setTo(to);
-        helper.setFrom("JoinGameDev@joingamedev.pl");
-        helper.setSubject("JoinGameDev.pl - Aktywacja konta użytkownika");
+        helper.setFrom("MCList@mc-list.pl");
+        helper.setSubject("MC-List.pl - Aktywacja konta użytkownika");
         helper.setText(activeAccountEmail, true);
 
         mailSender.send(message);
@@ -65,10 +65,10 @@ public class EmailSenderService {
                 + "    <div id='header' style='background-color: #1f1f7c; width: 100%; height: 250px;'></div>"
                 + "    <div id='panel' style='background-color: white; width: 700px; margin-left: auto; margin-right: auto; margin-top: -100px; border-radius: 5px;'>"
                 + "        <div id='logo' style='width: 250px; padding: 25px; margin-left: auto; margin-right: auto;'>"
-                + "            <img src='https://zapodaj.net/images/fb31beff19dd9.png' style='width: 100%; height: auto;'>"
+                + "            <img src='https://zapodaj.net/images/f86bc94eb0cbe.png' style='width: 100%; height: auto;'>"
                 + "        </div>"
                 + "        <div id='container' style='padding: 25px'>"
-                + "            Otrzymaliśmy prośbę o zresetowanie hasła do Twojego konta na stronie <span style='font-weight: 800;'>joingamedev.pl!</span><br><br><br>"
+                + "            Otrzymaliśmy prośbę o zresetowanie hasła do Twojego konta na stronie <span style='font-weight: 800;'>MC-List.pl!</span><br><br><br>"
                 + "            Jeśli chcesz zresetować hasło, kliknij poniższy przycisk:"
                 + "            <a href='"+resetPasswordLink+"' style='text-decoration: none;'>"
                 + "                <div id='button' style='background-color: #ffbe4d; padding: 10px 50px; width: fit-content; border-radius: 5px; color: white; font-weight: 800; margin-top: 20px; margin-bottom: 30px; margin-left: auto; margin-right: auto;'>"
@@ -82,7 +82,7 @@ public class EmailSenderService {
                 + "    </div>"
                 + "    <div id='panel-down' style='background-color: #d1d1d1; padding: 25px 50px; width: 600px; margin-left: auto; margin-right: auto; margin-top: 50px; margin-bottom: 150px; text-align: center;'>"
                 + "        Jeśli potrzebujesz pomocy, prosimy o kontakt <br>"
-                + "        <span style='font-weight: 800;'>support@joingamedev.pl</span>"
+                + "        <span style='font-weight: 800;'>support@coresaken.com</span>"
                 + "    </div>"
                 + "</body>"
                 + "</html>";
@@ -92,8 +92,8 @@ public class EmailSenderService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         helper.setTo(to);
-        helper.setFrom("JoinGameDev@joingamedev.pl");
-        helper.setSubject("JoinGameDev.pl - Resetowanie hasła");
+        helper.setFrom("MCList@mc-list.pl");
+        helper.setSubject("MC-List.pl - Resetowanie hasła");
         helper.setText(resetPasswordEmail, true);
 
         mailSender.send(message);
